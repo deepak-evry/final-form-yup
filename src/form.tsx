651c3a8bd701e56af488173b56950cefeaf13a04
deepak-evry <deepak.thayil@cbre.com>
2021-03-01 12:30:11 +0530

    Session taking updates

--- src/form.tsx
+++ src/form.tsx
@@ -2,42 +2,32 @@ import { Form, Field } from 'react-final-form';
 import { userSchema } from './validation/UserSchema'
 import { setIn } from 'final-form';
 import { User } from './types/user'; 
-import { getFakeData } from 'yup-faker';
+//import { getFakeData } from 'yup-faker';
 
 
 const onSubmit = async (values: User) => { 
-  const validated: User = userSchema.validateSync(values); 
+  const validated: User = await userSchema.validate(values); 
   window.alert(JSON.stringify(validated));
   }
-const validateFormValues = (schema: any  ) => async (values:any) => {
-  if (typeof schema === 'function') {
-    schema = schema();
-  }
+
+
+const validateFormValues = async (values:any) => { 
   try {
-    await schema.validate(values, { abortEarly: false });
+    await userSchema.validate(values,{ abortEarly: false });
   } catch (err) {
     const errors = err.inner.reduce((formError:any, innerError:any) => {
       return setIn(formError, innerError.path, innerError.message);
     }, {});
-
     return errors;
   }
 };
-const validate = validateFormValues(userSchema);
-
-// const InitialValues : User = {
-//   firstName : 'Deepak',
-//   lastNamne : 'Thayyil',
-//   phoneNumber : 8147204209,
-// }
-
-const InitialValues = getFakeData(userSchema);
-
+ 
+const InitialValues = {};
 
 export const MyForm = () => (
   <Form
     onSubmit={onSubmit}
-    validate={validate}
+    validate={validateFormValues}
     initialValues={InitialValues}
     render={({ handleSubmit,submitError }) => (
       <form onSubmit={handleSubmit}> 
@@ -49,7 +39,7 @@ export const MyForm = () => (
                 <input {...input} type="text" placeholder="firstName" />
                 {meta.error && meta.touched && <span>{meta.error}</span>}
               </div>
-            )}
+            )}  
           </Field>
         </div> 
         <div>
@@ -78,6 +68,24 @@ export const MyForm = () => (
               <div>
                 <label>email</label>
                 <input {...input} type="email" placeholder="email" />
+                {meta.error && meta.touched && <span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+          <Field name="isDescrition">
+            {({ input, meta }) => (
+              <div>
+                <label>Descrition</label>
+                <input {...input} type="checkbox" placeholder="email" />
+                {meta.error && meta.touched && <span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+          <Field name="description">
+            {({ input, meta }) => (
+              <div>
+                <label>Description</label>
+                <input {...input} type="description" placeholder="description" />
                 {meta.error && meta.touched && <span>{meta.error}</span>}
               </div>
             )}
--- src/validation/UserSchema.ts
+++ src/validation/UserSchema.ts
@@ -1,12 +1,20 @@
 import * as yup from 'yup';
 
+//const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+
 export const userSchema = yup.object({
-    firstName: yup.string().required(),
-    lastNamne: yup.string().notRequired(),
+    firstName: yup.string().required('Please enter first name'),
+    lastName: yup.string().notRequired(),
     phoneNumber: yup.number()
-    .required(),
-    // .matches(/^[0-9]+$/, "Must be only digits")
-    // .min(10, 'Must be exactly 5 digits')
-    // .max(10, 'Must be exactly 5 digits')
+    .typeError("enter valid phone number"),
+    //  yup.string().required("required")
+    // .matches(phoneRegExp, 'Phone number is not valid')
+    // .min(10, "to short")
+    // .max(10, "to long"),
     email:yup.string().email(),
-});
+    isDescrition :yup.boolean().default(false),
+    description : yup.string().when('isDescrition',(isDescrition:boolean)=>{
+        return  isDescrition?  yup.string().required() : yup.string().notRequired()
+    })
+    
+})
